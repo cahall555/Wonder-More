9f0e13b1727b494ae3156ea79b5fe58e88c5b8eb
cahall555 <cahall555@gmail.com>
2017-04-18 15:32:45 -0600

    created dictionary

--- First.py
+++ First.py
@@ -6,9 +6,20 @@ shoplist.append("Milk")
 shoplist.append("Butter")
 shoplist.append("Apples")
 print(shoplist)
+#try a function
 def first_function():
 	print("Hello From First Function!")
 print(first_function)
-
-
+#create a dictionary
+first_dictionary = {
+	"Operouse" : "tedious",
+	"Callow" : "lacking adult sophistication",
+	"Gamification" : "the process of adding games",
+	"Prestidigitation" : "slide of hand"
+}
+print(first_dictionary)
+#for loop
+for word, definition in first_dictionary.items():
+	print("The word of the day is %s : %s" % (word, definition))
+	
 
