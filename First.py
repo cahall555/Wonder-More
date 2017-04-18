5c5ab678edd9a5d07efe01e1c1e829da3b43a55d
cahall555 <cahall555@gmail.com>
2017-04-18 14:24:31 -0600

    added first function

--- First.py
+++ First.py
@@ -6,6 +6,9 @@ shoplist.append("Milk")
 shoplist.append("Butter")
 shoplist.append("Apples")
 print(shoplist)
+def first_function():
+	print("Hello From First Function!")
+print(first_function)
 
 
 
