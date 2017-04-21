ca29608bcbaf95b4807d1cfd890b4d01253575b8
cahall555 <cahall555@gmail.com>
2017-04-21 16:48:40 -0600

    added begining of flashcard game

---
+++ py.kid/flashcards.py
@@ -0,0 +1,7 @@
+#Flashcard game
+#First ask user how many flashcards they want to enter.
+#Then loop through desired quantity
+fc_num = input("How many flashcards would you like to make?")
+a = 0
+while a < fc_num:
+	card.fc_num = 
