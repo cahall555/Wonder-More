2344347a95e2c7cf1e7924489fa74389026e0641
Courtney <cahall555@gmail.com>
2017-04-21 21:53:56 -0600

    loop asking for cards 1- ?

--- py.kid/flashcards.py
+++ py.kid/flashcards.py
@@ -1,7 +1,18 @@
 #Flashcard game
 #First ask user how many flashcards they want to enter.
 #Then loop through desired quantity
-fc_num = input("How many flashcards would you like to make?")
-a = 0
-while a < fc_num:
-	card.fc_num = 
+fc_num = int(input("How many flashcards would you like to make?"))
+a = int(1)
+
+while (a <= fc_num):
+	print("Card %s of %s" % (a,fc_num))
+	class myclass:
+	side1 = input("what would you like on side 1?")
+	side2 = input("what would you like on side 2?")
+	a = a + 1
+
+# what order would you like the cards, suffled, not shuffled
+#then print the cards
+
+print(side1)
+print(side2)
