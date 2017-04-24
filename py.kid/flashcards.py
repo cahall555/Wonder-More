d6a7d087638e924a3d8e796c16911aea667d5710
cahall555 <cahall555@gmail.com>
2017-04-24 16:11:59 -0600

    working with creating dynamic variables with dictionary and class.

--- py.kid/flashcards.py
+++ py.kid/flashcards.py
@@ -4,9 +4,20 @@
 fc_num = int(input("How many flashcards would you like to make?"))
 a = int(1)
 
+#After getting the number of flashcards a user wants dictionary loop should
+#loop user through adding flascards. **currently working on dictionary method. 
 while (a <= fc_num):
 	print("Card %s of %s" % (a,fc_num))
-	class myclass:
+	flashcards = {}
+	class card:
+		def __init__(self,card):
+			self.card = card
+		def side1(self):
+			for line in self.card:
+				print
+		def side2 (self):
+			for line in self.card:
+				print
 	side1 = input("what would you like on side 1?")
 	side2 = input("what would you like on side 2?")
 	a = a + 1
