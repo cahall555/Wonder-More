80da823248d8cc5e030e5f11e4193ca662661642
Blackbird <blackbird@blackbird.domain>
2017-06-25 11:37:11 -0600

    updated flashcard game

--- py.kid/flashcards.py
+++ py.kid/flashcards.py
@@ -1,32 +1,28 @@
 #Flashcard game
 #First ask user how many flashcards they want to enter.
-#Then loop through desired quantity
+#To loop through desired quantity
 fc_num = int(input("How many flashcards would you like to make?"))
 a = int(1)
 
+#setup empty dictionary to hold side one and side 2 of flash cards
+fc_dictionary = {}
+
 #After getting the number of flashcards a user wants dictionary loop should
-#loop user through adding flascards. **currently working on dictionary method. 
+#loop user through adding flascards.  
 while (a <= fc_num):
-	print("Card %s of %s" % (a,fc_num))
-	class card:
-		def __init__(self,card):
-			self.card = card
-		def side1(self):
-			for line in self.card:
-				print
-		def side2 (self):
-			for line in self.card:
-				print
-	flashcards{}
-	side1 = input("what would you like on side 1?")
-	side2 = input("what would you like on side 2?")
-	
-	for fc_num in range(fc_num)):
-		flashcards[side1[fc_num]] = card[fc_num]
-	a = a + 1
+    print("Card %s of %s" % (a,fc_num))
+    k = raw_input("what would you like on side 1? ")
+    v = raw_input("what would you like on side 2? ")
+    fc_dictionary[k] = v
+    a = a + 1
+
+#print out flashcards so user see entries
+for key, value in fc_dictionary.items():
+    print("\nSide 1: " + key)
+    print("Side 2: " + value)
+
 
 # what order would you like the cards, suffled, not shuffled
 #then print the cards
 
-print(side1)
-print(side2)
+
