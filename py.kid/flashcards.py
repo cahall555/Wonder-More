262237c186d63c211da195bffd659bac7104a4ad
Courtney <cahall555@gmail.com>
2017-07-18 19:44:58 -0600

    update

--- py.kid/flashcards.py
+++ py.kid/flashcards.py
@@ -1,32 +1,25 @@
+#Flashcard game
+#First ask user how many flashcards they want to enter.
+#To loop through desired quantity
+fc_num = int(input("How many flashcards would you like to make?"))
+a = int(1)
 
-from kivy.app import App
+#setup empty dictionary to hold side one and side 2 of flash cards
+fc_dictionary = {}
 
-class FcApp (App):
-    #Flashcard game
-    #First ask user how many flashcards they want to enter.
-    #To loop through desired quantity
-    fc_num = int(input("How many flashcards would you like to make?"))
-    a = int(1)
+#After getting the number of flashcards a user wants dictionary loop should
+#loop user through adding flascards.  
+while (a <= fc_num):
+    print("Card %s of %s" % (a,fc_num))
+    k = input("what would you like on side 1? ")
+    v = input("what would you like on side 2? ")
+    fc_dictionary[k] = v
+    a = a + 1
+#print out flashcards so user see entries
+for key, value in fc_dictionary.items():
+    print("\nSide 1: " + key)
+    print("Side 2: " + value)
 
-    #setup empty dictionary to hold side one and side 2 of flash cards
-    fc_dictionary = {}
-
-    #After getting the number of flashcards a user wants dictionary loop should
-    #loop user through adding flascards.  
-    while (a <= fc_num):
-        print("Card %s of %s" % (a,fc_num))
-        k = raw_input("what would you like on side 1? ")
-        v = raw_input("what would you like on side 2? ")
-        fc_dictionary[k] = v
-        a = a + 1
-
-    #print out flashcards so user see entries
-    for key, value in fc_dictionary.items():
-        print("\nSide 1: " + key)
-        print("Side 2: " + value)
-
-if __name__ == '__main__':
-    FcApp().run()
 # what order would you like the cards, suffled, not shuffled
 #then print the cards
 
