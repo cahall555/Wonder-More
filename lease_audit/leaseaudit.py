a2bfcf53cf4ab931b107f04da2cc3e8011e8b158
Courtney <cahall555@gmail.com>
2018-04-19 07:59:56 -0600

    add beginning of function

--- lease_audit/leaseaudit.py
+++ lease_audit/leaseaudit.py
@@ -1,9 +1,15 @@
 import PyPDF2
 import pandas as pd
 import numpy as np
+def find_parameters(parameters)
+    """ search for lease parameters""""
+    for parameter in parameters:
+        """input search loop here"""
+parameters = ['Resident','term date']
 file = open('21step1.pdf','rb')
 reader = PyPDF2.PdfFileReader(file)
 page = reader.getPage(0)
 lease = page.extractText()
 leasel = lease.split()
 leasel = pd.DataFrame(leasel)
+find_parameters(parameters)
